8dddab88e3d77d72228ee621bb21479d3c727248
ZuXianA <67884281+ZuXianA@users.noreply.github.com>
2022-11-27 16:26:35 +0800

    Update nonlinear_optics_1.m

--- nonlinear_optics/nonlinear_optics_1.m
+++ nonlinear_optics/nonlinear_optics_1.m
@@ -2,21 +2,31 @@ clear all;close all;clc;
 
 x = linspace(0.7,2,1000);   % 波长范围 0.7~2 um
 
+% figure(1);
+% subplot(211),plot(x,Sellmier(x)),title('Sellmier'),grid on;
+% xlabel('\lambda  /  \mum'),ylabel('index of refraction');
+% subplot(212),plot(x,Newton(x)),title('Newton'),grid on;
+% xlabel('\lambda  /  \mum'),ylabel('index of refraction');
+
+figure(2);
 subplot(221),plot(x,Sellmier(x)),title('Sellmier'),grid on;
 xlabel('\lambda  /  \mum'),ylabel('index of refraction');
 subplot(223),plot(x,Newton(x)),title('Newton'),grid on;
 xlabel('\lambda  /  \mum'),ylabel('index of refraction');
 
-D = dispersionD(x);
+[D,p1] = dispersionD(x);
 subplot(222),plot(x(3:end),D,'b',x(3:end),zeros(length(x(3:end))),'r--');
 title('$D=-\frac{\lambda}{c}\frac{d^2n}{d\lambda^2}$','Interpreter','latex');
 xlabel('\lambda  /  \mum'),ylabel('色散参量 D');grid on;
 text(0.8,-5e-17,'正常色散区',FontSize=13);
 text(1.6,3e-17,'反常色散区',FontSize=13);
 
-subplot(224),plot(x(3:end),DeltaTao(x));
+[deltaTao,v,p] = DeltaTao(x,x(p1(1)+2));
+subplot(224),plot(x(3:end),deltaTao,[x(p+3),x(p+3)],[0,18e-17],'r--');
 title("\Delta\tau = D*\Delta\lambda");grid on;
 xlabel('\lambda  /  \mum'),ylabel('时延差 \Delta\tau');
+
+fprintf('D0 = %g\n', x(p1(1)+2));
 
 function n = Sellmier(wavelength)       % 构造 Sellmeier 公式
 B1 = 0.6961663;
@@ -36,14 +46,16 @@ c3 = 0.00292;
 n = c1 + c2*wavelength.^2 + c3*wavelength.^(-2);
 end
 
-function D = dispersionD(wavelength)     % 色散参量 D 的波长表达式，图标题写清楚了具体形式
-c = 299792458;   % 光速
+function [D,p] = dispersionD(wavelength)     % 色散参量 D 的波长表达式，图标题写清楚了具体形式
+c = 299792458;   % 光速                      % p 是 D=0 所在位置的下标
 nn = Sellmier(wavelength);
 d2n = diff(nn,2);
 D = -wavelength(3:end) .* d2n / c;
+p = find(D>=0);
 end
 
-function deltaTao = DeltaTao(wavelength)    % 归一化时延差用色散参量表示，图标题写清楚了具体形式
-DeltaLamda = 100;
-deltaTao = dispersionD(wavelength) * DeltaLamda;
+function [deltaTao,v,p] = DeltaTao(wavelength,D0)    % 归一化时延差用色散参量表示，图标题写清楚了具体形式
+DeltaLamda = wavelength(3:end) - D0;
+deltaTao = dispersionD(wavelength) .* DeltaLamda;
+[v,p] = min(deltaTao>0);
 end
